057b097ced35b7d773a57a42aa5cbb2a11bac3f3
Kardam-Codes <kardam1600@example.com>
2026-02-13 14:57:29 +0530

    refactor: migrate TypeScript to JavaScript and rename templates to B_Templates

---
+++ B_Templates/services/admin.service.js
@@ -0,0 +1,21 @@
+/**
+ * FILE.service.ts
+ * OWNER
+ *
+ * PURPOSE:
+ * - Reusable TEMPLATE for hackathons & fast builds
+ *
+ * YOU SHOULD:
+ * - Implement the simplest working version
+ * - Keep defaults predictable
+ * - Make it reusable across projects
+ *
+ * DO NOT:
+ * - Add business-specific logic
+ * - Over-engineer
+ * - Optimize prematurely
+ *
+ * NOTES:
+ * - This file can be extended or deleted later
+ * - Clarity > Cleverness
+ */
